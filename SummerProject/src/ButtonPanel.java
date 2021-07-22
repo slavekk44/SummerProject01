7531813b7585847314a485f385efbdff7625f464
TheDoss <TheDoss@DESKTOP-DOQSQLL>
2021-07-22 23:38:12 +0100

    buttons problem

--- SummerProject/src/ButtonPanel.java
+++ SummerProject/src/ButtonPanel.java
@@ -1,14 +1,16 @@
-import java.awt.BorderLayout;
+
 import java.awt.Color;
-import java.awt.Dimension;
+
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 
 
-public class ButtonPanel extends JPanel {
-
+public class ButtonPanel extends JPanel implements ActionListener {
+	boolean check = true;
 	private JButton cart1;
 	public int size;
 	public ButtonPanel(String buttonName, Color color) {
@@ -21,12 +23,29 @@ public class ButtonPanel extends JPanel {
 		cart1.setOpaque(true);
 		cart1.setBackground(color);
 		cart1.setBorder(BorderFactory.createLineBorder(Color.WHITE, 10));
-		
+		this.cart1.addActionListener(this);
 		
 		
 		cart1.setBorder(compound);
 		add(cart1);
 //		
 		}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+if(e.getSource() == cart1) {
+	
+	if(check == true) {
+	cart1.setBackground(Color.GREEN);
+	check = false;
+	}
+	else {
+		cart1.setBackground(Color.WHITE);
+		check = true;
+	}
+		}
+
+	}
 	}
 
